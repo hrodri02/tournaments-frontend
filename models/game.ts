3d53f02f759893a790b31e15c50089f777e209ab
Heri <hrodriguez1821@gmail.com>
2024-08-01 14:44:11 -0700

    Added an id property to the game class.

--- models/game.ts
+++ models/game.ts
@@ -1,9 +1,11 @@
 export class Game {
+    id: number;
     location: string;
     startTime: string;
     durationInHours: number;
 
-    constructor(location: string, startTime: string, durationInHours: number) {
+    constructor(id: number, location: string, startTime: string, durationInHours: number) {
+        this.id = id;
         this.location = location;
         this.startTime = startTime;
         this.durationInHours = durationInHours;
