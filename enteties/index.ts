74ebd0a1557770c812b24f38123739332012cd90
Heri <hrodriguez1821@gmail.com>
2024-08-06 10:17:22 -0700

    Added a file to represent all the app objects as interfaces. #3

---
+++ enteties/index.ts
@@ -0,0 +1,36 @@
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+}
+
+export interface Player extends User {
+    position: number;
+}
+
+export interface Team {
+    name: string;
+    players: Player[];
+}
+
+export interface GameStat {
+    goal: number;
+    player: Player;
+    time: Date;
+}
+
+export interface Game {
+    homeTeam: Team;
+    awayTeam: Team;
+    date: Date;
+    stats: GameStat[];
+}
+
+export interface League {
+    name: string;
+    // This property might be better as an enum with values (open, closed, ongoing, ended)
+    status: number;
+    teams: Team[];
+    games: Game[];
+}
+
