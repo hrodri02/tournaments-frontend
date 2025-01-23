62814ef0f44a866808f98b7e3f6be623f35720f5
Heri <hrodriguez1821@gmail.com>
2025-01-23 10:13:09 -0800

    The date properties are now strings to be able to store them in redux store and the status property of a league is enum, so I changed generateMockLeagues to generate a random value for this property.

--- services/tournaments.service.ts
+++ services/tournaments.service.ts
@@ -1,5 +1,5 @@
 import {faker} from '@faker-js/faker';
-import {Game, GameStat, GameStatType, League, Player, Team} from "@/entities";
+import {Game, GameStat, GameStatType, League, LeagueStatus, Player, Team} from "@/entities";
 
 const generateMockPlayers = (count: number): Player[] => {
     return Array.from({length: count}, (_, id) => ({
@@ -23,7 +23,7 @@ const generateMockGameStats = (players: Player[], count: number): GameStat[] => 
         id: id + 1,
         type: faker.helpers.enumValue(GameStatType),
         player: players[faker.number.int({min: 0, max: players.length - 1})],
-        time: faker.date.past()
+        time: faker.date.past().toISOString()
     }));
 };
 
@@ -35,7 +35,6 @@ const generateMockGames = (teams: Team[], count: number): Game[] => {
         homeTeam: homeTeam,
         awayTeam: awayTeam,
         address: faker.location.streetAddress(),
-        date: faker.date.future(),
         stats: generateMockGameStats(homeTeam.players.concat(awayTeam.players), 5)
     }));
 };
@@ -44,7 +43,8 @@ const generateMockLeagues = (count: number, teamsPerLeague: number, gamesPerLeag
     return Array.from({length: count}, (_, id) => ({
         id: id + 1,
         name: faker.company.name(),
-        status: faker.number.int({min: 0, max: 3}), // 0 = open, 1 = closed, 2 = ongoing, 3 = ended, these are just some ideas
+        date: faker.date.future().toISOString(),
+        status: faker.helpers.enumValue(LeagueStatus), 
         teams: generateMockTeams(teamsPerLeague, 15),
         games: generateMockGames(generateMockTeams(teamsPerLeague, 15), gamesPerLeague)
     }));
