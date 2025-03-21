13d9986ba23f2be18fa5543de9d5778744a230be
Heri <hrodriguez1821@gmail.com>
2025-03-21 12:48:20 -0700

    Added the jws and error field to the auth state. Added a thunk to represent a loginRequest, added handlers in createSlice, and selectors for fields of auth state.

--- store/auth.slice.ts
+++ store/auth.slice.ts
@@ -1,22 +1,64 @@
 import {createSlice, PayloadAction} from "@reduxjs/toolkit";
 import {User} from "@/entities/auth";
+import { createAppAsyncThunk } from '@/hooks/useStore';
+import { login } from '@/services/auth.service';
+import { RootState } from '@/store/store'
 
-interface State {
+interface AuthState {
+    jws: string | null;
     user: User | null;
-    status: 'idle' | 'loading' | 'failed';
+    status: 'idle' | 'loading' | 'succeeded' | 'failed';
+    error: string | null; 
 }
 
-const initialState: State = {
+const initialState: AuthState = {
+    jws: null,
     user: null,
     status: 'idle',
+    error: null
 }
+
+export const loginRequest = createAppAsyncThunk(
+  'auth/login',
+  async ({ email, password }: { email: string; password: string }) => {
+    const {token, user} = await login(email, password)
+    return user
+  },
+  {
+    condition(arg, thunkApi) {
+      const authStatus = selectAuthStatus(thunkApi.getState())
+      if (authStatus !== 'idle') {
+        return false
+      }
+    }
+  }
+)
 
 const slice = createSlice({
     name: 'auth',
     initialState,
     reducers: {},
+    extraReducers: builder => {
+        builder
+        .addCase(loginRequest.pending, (state, action) => {
+            state.status = 'loading'
+        })
+        .addCase(loginRequest.fulfilled, (state, action) => {
+            state.status = 'succeeded'
+            state.user = action.payload;
+            console.log('current user:', state.user);
+        })
+        .addCase(loginRequest.rejected, (state, action) => {
+            state.status = 'failed'
+            state.error = action.error.message ?? 'Unknown Error'
+        })
+    }
 })
 
 export const {} = slice.actions
 
 export default slice.reducer
+
+export const selectAuthUser = (state: RootState) => state.auth.user
+export const selectAuthStatus = (state: RootState) => state.auth.status
+export const selectAuthError = (state: RootState) => state.auth.error
