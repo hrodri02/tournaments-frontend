5b95bc5585ee5a5c4bfc086e575e236aca331a31
Heri <hrodriguez1821@gmail.com>
2025-03-21 12:50:56 -0700

    Changed what is being displayed depending on the login request. For an idle state the regual screen shows, for the loading state a loading spinner shows, for the succeeded state the user is redirected to home screen, and for the failed state and error message is shown.

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,49 +1,78 @@
-import React, { useState } from 'react';
-import { Link } from 'expo-router';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Link, Redirect } from 'expo-router';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, KeyboardAvoidingView, ActivityIndicator } from 'react-native';
+import { useAppSelector, useAppDispatch } from '@/hooks/useStore';
+import { loginRequest, selectAuthStatus, selectAuthError } from '@/store/auth.slice';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 const LoginScreen = () => {
+    const dispatch = useAppDispatch()
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const authStatus = useAppSelector(selectAuthStatus);
+    const authError = useAppSelector(selectAuthError);
+
+    let content: React.ReactNode
+    if (authStatus === "idle") {
+        content = <KeyboardAvoidingView style={styles.container} behavior="padding">
+                    <View style={styles.innerContainer}>
+                        <Text style={styles.title}>Welcome Back!</Text>
+                        <Text style={styles.subtitle}>Login to your account</Text>
+
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Email"
+                            value={email}
+                            onChangeText={text => setEmail(text)}
+                            keyboardType="email-address"
+                            autoCapitalize="none"
+                            placeholderTextColor="#6e6e6e"
+                        />
+
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Password"
+                            value={password}
+                            onChangeText={text => setPassword(text)}
+                            secureTextEntry
+                            placeholderTextColor="#6e6e6e"
+                        />
+
+                        <TouchableOpacity style={styles.button} onPress={() => 
+                            dispatch(loginRequest({email, password}))
+                        }>
+                            <Text style={styles.buttonText}>Login</Text>
+                        </TouchableOpacity>
+
+                        <Text style={styles.footerText}>
+                            Don't have an account? <Link href="/auth/signup" style={styles.linkText}>Sign Up</Link>
+                        </Text>
+                    </View>
+                </KeyboardAvoidingView>
+    }
+    if (authStatus === "loading") {
+        content = <ActivityIndicator size="large" color="#0000ff"/>
+    }
+    else if (authStatus === "succeeded") {
+        content = <Redirect href="/" />
+    }
+    else if (authStatus === "failed") {
+        content = <Text style={styles.errorView}>{authError}</Text>
+    }
 
     return (
-        <KeyboardAvoidingView style={styles.container} behavior="padding">
-            <View style={styles.innerContainer}>
-                <Text style={styles.title}>Welcome Back!</Text>
-                <Text style={styles.subtitle}>Login to your account</Text>
-
-                <TextInput
-                    style={styles.input}
-                    placeholder="Email"
-                    value={email}
-                    onChangeText={text => setEmail(text)}
-                    keyboardType="email-address"
-                    autoCapitalize="none"
-                    placeholderTextColor="#6e6e6e"
-                />
-
-                <TextInput
-                    style={styles.input}
-                    placeholder="Password"
-                    value={password}
-                    onChangeText={text => setPassword(text)}
-                    secureTextEntry
-                    placeholderTextColor="#6e6e6e"
-                />
-
-                <TouchableOpacity style={styles.button} onPress={() => console.log('Login pressed')}>
-                    <Text style={styles.buttonText}>Login</Text>
-                </TouchableOpacity>
-
-                <Text style={styles.footerText}>
-                    Don't have an account? <Link href="/auth/signup" style={styles.linkText}>Sign Up</Link>
-                </Text>
-            </View>
-        </KeyboardAvoidingView>
+        <SafeAreaProvider>
+            <SafeAreaView style={styles.safeAreaView}>
+                {content}
+            </SafeAreaView>
+        </SafeAreaProvider>
     );
 };
 
 const styles = StyleSheet.create({
+    safeAreaView: {
+        flex: 1,
+    },
     container: {
         flex: 1,
         justifyContent: 'center',
@@ -96,6 +125,9 @@ const styles = StyleSheet.create({
         color: '#6200ee',
         fontWeight: 'bold',
     },
+    errorView: {
+        textAlign: 'center'
+    }
 });
 
 export default LoginScreen;
