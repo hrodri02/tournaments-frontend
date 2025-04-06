5878d02e472460d8e793625dbafd94e7f8ff553f
Angel Roma <angelr10mar@gmail.com>
2025-04-06 02:47:54 -0600

    Refactor AppLayout component to remove color scheme dependency and simplify tab bar configuration.

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,18 +1,14 @@
 import { Tabs } from "expo-router";
-import { useColorScheme } from "react-native";
-import { Colors } from "@/constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
 import ProtectedRoute from "@/components/ProtectedRoute";
 
 export default function AppLayout() {
-  const colorScheme = useColorScheme();
 
   return (
     <ProtectedRoute>
       <Tabs
         screenOptions={{
           headerShown: false,
-          tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         }}
       >
         {/* This is needed to hide the index tab */}
