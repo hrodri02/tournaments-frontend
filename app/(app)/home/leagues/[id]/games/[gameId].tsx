32d5e47354b36efb7e732f3590c1f551ad6b8a2d
Heri <hrodriguez1821@gmail.com>
2025-07-09 10:38:29 -0700

    Display game data in redux instead of hardcoded values.

--- app/(app)/home/leagues/[id]/games/[gameId].tsx
+++ app/(app)/home/leagues/[id]/games/[gameId].tsx
@@ -5,6 +5,8 @@ import { useNavigation } from '@react-navigation/native';
 import { useLocalSearchParams } from 'expo-router';
 import { useAppSelector } from '@/hooks/useStore';
 import { selectGameById, selectLeagueById } from '@/store/leagues/leaguesSlice';
+import { format } from 'date-fns';
+import { GameStatType } from '@/entities';
 
 const screenHeight = Dimensions.get('window').height; 
 
@@ -13,19 +15,52 @@ export default function Game() {
     const leagueId = Number(id);
     const gameID = Number(gameId);
     const league = useAppSelector(state => selectLeagueById(state, leagueId));
-    const game = useAppSelector(state => selectGameById(league, gameID))
+    const game = useAppSelector(state => selectGameById(league, gameID))!
+    const homeTeam = game.homeTeam
+    const awayTeam = game.awayTeam
+    const date = Date.parse(game.date);
+    const formattedDate = format(date, 'eee, MMM i');
+    const goalStats = game.stats.filter((gameStat) => gameStat.type === GameStatType.goal)
+    const homeTeamGoalStats = goalStats.filter((stat) => homeTeam.players.includes(stat.player) );
+    const homeTeamGoalScorers = homeTeamGoalStats.reduce((accumulator, stat) => {
+        return (accumulator === '')? stat.player.name : accumulator + ', ' + stat.player.name
+    }, '')
+    const awayTeamGoalStats = goalStats.filter((stat) => awayTeam.players.includes(stat.player) );
+    const awayTeamGoalScorers = awayTeamGoalStats.reduce((accumulator, stat) => {
+        return (accumulator === '')? stat.player.name : accumulator + ', ' + stat.player.name
+    }, '')
+    const homeTeamYellowCards = game.stats.reduce((numCards, stat) => {
+        if (stat.type === GameStatType.yellowCard && homeTeam.players.includes(stat.player))
+            return numCards += 1
+        return numCards
+    }, 0)
+    const awayTeamYellowCards = game.stats.reduce((numCards, stat) => {
+        if (stat.type === GameStatType.yellowCard && awayTeam.players.includes(stat.player))
+            return numCards += 1
+        return numCards
+    }, 0)
+    const homeTeamRedCards = game.stats.reduce((numCards, stat) => {
+        if (stat.type === GameStatType.redCard && homeTeam.players.includes(stat.player))
+            return numCards += 1
+        return numCards
+    }, 0)
+    const awayTeamRedCards = game.stats.reduce((numCards, stat) => {
+        if (stat.type === GameStatType.redCard && awayTeam.players.includes(stat.player))
+            return numCards += 1
+        return numCards
+    }, 0)
     
     const navigation = useNavigation(); 
     useLayoutEffect(() => { 
         navigation.setOptions({
-            title: 'HomeTeam vs AwayTeam',
+            title: `${homeTeam.name} vs ${awayTeam.name}`,
             headerTitleAlign: 'center'
         }); 
     }, [navigation]);
 
     return (
         <SafeAreaView>
-            <Text style={styles.date}>Monday, March 17 at Golden Gate Park</Text>
+            <Text style={styles.date}>{formattedDate} at {game.address}</Text>
             
             <View style={styles.gameStatView}> 
                 <View style={styles.teamLogo}> 
@@ -34,11 +69,11 @@ export default function Game() {
                         source={require('@/assets/images/liga_mx_logo.jpeg')}
                         resizeMode='contain'
                     />
-                    <Text style={styles.text}>HomeTeam </Text>
+                    <Text style={styles.text}>{homeTeam.name}</Text>
                 </View>
 
                 <View style={styles.equalWidth}>
-                    <Text style={styles.score}> 1 - 0 </Text>
+                    <Text style={styles.score}> {homeTeamGoalStats.length} - {awayTeamGoalStats.length} </Text>
                 </View>
 
                 <View style={styles.teamLogo}> 
@@ -47,30 +82,30 @@ export default function Game() {
                         source={require('@/assets/images/liga_mx_logo.jpeg')}
                         resizeMode='contain'
                     />
-                    <Text style={styles.text}>AwayTeam</Text>
+                    <Text style={styles.text}>{awayTeam.name}</Text>
                 </View>
             </View>
 
             <View style={styles.gameStatView}>
-                <Text style={[styles.text, styles.goalsViewItem]}> Raul Jimenez </Text>
+                <Text style={[styles.text, styles.goalsViewItem]}>{homeTeamGoalScorers}</Text>
                 <Image 
                     style={[styles.goalsViewItem, styles.soccerBallImage]} 
                     source={require('@/assets/images/soccerBall.png')}
                     resizeMode='contain'
                 />
-                <Text style={[styles.text, styles.goalsViewItem]}> Raul Jimenez </Text>
+                <Text style={[styles.text, styles.goalsViewItem]}>{awayTeamGoalScorers}</Text>
             </View>
 
             <View style={styles.gameStatView} >
-                <Text style={[styles.text, styles.equalWidth]}> 1 </Text>
+                <Text style={[styles.text, styles.equalWidth]}>{homeTeamYellowCards}</Text>
                 <Text style={[styles.text, styles.equalWidth]}> Yellow Cards </Text>
-                <Text style={[styles.text, styles.equalWidth]}> 2 </Text>
+                <Text style={[styles.text, styles.equalWidth]}>{awayTeamYellowCards}</Text>
             </View>
 
             <View style={styles.gameStatView}> 
-                <Text style={[styles.text, styles.equalWidth]}> 0 </Text>
+                <Text style={[styles.text, styles.equalWidth]}>{homeTeamRedCards}</Text>
                 <Text style={[styles.text, styles.equalWidth]}> Red Cards </Text>
-                <Text style={[styles.text, styles.equalWidth]}> 0 </Text>
+                <Text style={[styles.text, styles.equalWidth]}>{awayTeamRedCards}</Text>
             </View>
         </SafeAreaView>
     );
