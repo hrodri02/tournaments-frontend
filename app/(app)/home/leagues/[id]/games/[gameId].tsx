d25927ee920bdb325e542012e7cc731508994fc8
Heri <hrodriguez1821@gmail.com>
2025-07-24 15:42:13 -0700

    Displaying game stats obtained from backend.

--- app/(app)/home/leagues/[id]/games/[gameId].tsx
+++ app/(app)/home/leagues/[id]/games/[gameId].tsx
@@ -1,10 +1,12 @@
-import React, { useLayoutEffect, useState, useEffect, useCallback } from 'react'; 
-import { StyleSheet, View, Text, Image, Dimensions, TouchableOpacity, Modal } from 'react-native';
+import React, { useLayoutEffect, useState, useEffect, useCallback, useMemo } from 'react'; 
+import { StyleSheet, View, Text, Image, Dimensions, TouchableOpacity, ActivityIndicator, Modal } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context'; 
 import { useNavigation } from '@react-navigation/native'; 
 import { useLocalSearchParams } from 'expo-router';
 import { useAppSelector, useAppDispatch } from '@/hooks/useStore';
-import { selectGameById, selectLeagueById, gameStatAdded } from '@/store/leagues/leaguesSlice';
+import { gameStatAdded } from '@/store/leagues/leaguesSlice';
+import { selectGameById } from '@/store/games/gamesSlice';
+import { fetchGameStats, makeSelectGameStatsByGameId, selectGameStatsStatus } from '@/store/gamestats/gameStatsSlice';
 import { getStorageItemAsync, USER_KEY } from '@/store/auth/authStorage';
 import { format } from 'date-fns';
 import { GameStatType, filterStatsForTeam, countStatsForTeam, getGoalScorersForTeam, stringToGameStatType, GameStatPayload } from '@/entities';
@@ -22,20 +24,25 @@ export default function Game() {
     const { id, gameId } = useLocalSearchParams();
     const leagueId = Number(id);
     const gameID = Number(gameId);
-    const league = useAppSelector(state => selectLeagueById(state, leagueId));
-    const game = useAppSelector(state => selectGameById(league, gameID))!
+    const game = useAppSelector(state => selectGameById(state, gameID))!
     const homeTeam = game.homeTeam
     const awayTeam = game.awayTeam
-    const date = Date.parse(game.date);
-    const formattedDate = format(date, 'eee, MMM i');
-    const homeTeamGoalStats = filterStatsForTeam(game.stats, GameStatType.goal, homeTeam)
+    const date = Date.parse(game.gameDateTime)
+    const formattedDate = format(date, 'eee, MMM i')
+    const gameStatsStatus = useAppSelector(selectGameStatsStatus)
+    const selectGameStatsOfGame = useMemo(
+        () => makeSelectGameStatsByGameId(gameID),
+        []
+    )
+    const gameStatsOfGame = useAppSelector(selectGameStatsOfGame)
+    const homeTeamGoalStats = filterStatsForTeam(gameStatsOfGame, GameStatType.goal, homeTeam)
     const homeTeamGoalScorers = getGoalScorersForTeam(homeTeamGoalStats)
-    const awayTeamGoalStats = filterStatsForTeam(game.stats, GameStatType.goal, awayTeam)
+    const awayTeamGoalStats = filterStatsForTeam(gameStatsOfGame, GameStatType.goal, awayTeam)
     const awayTeamGoalScorers = getGoalScorersForTeam(awayTeamGoalStats)
-    const homeTeamYellowCards = countStatsForTeam(game.stats, GameStatType.yellowCard, homeTeam)
-    const awayTeamYellowCards = countStatsForTeam(game.stats, GameStatType.yellowCard, awayTeam)
-    const homeTeamRedCards = countStatsForTeam(game.stats, GameStatType.redCard, homeTeam)
-    const awayTeamRedCards = countStatsForTeam(game.stats, GameStatType.redCard, awayTeam)
+    const homeTeamYellowCards = countStatsForTeam(gameStatsOfGame, GameStatType.yellowCard, homeTeam)
+    const awayTeamYellowCards = countStatsForTeam(gameStatsOfGame, GameStatType.yellowCard, awayTeam)
+    const homeTeamRedCards = countStatsForTeam(gameStatsOfGame, GameStatType.redCard, homeTeam)
+    const awayTeamRedCards = countStatsForTeam(gameStatsOfGame, GameStatType.redCard, awayTeam)
     
     const checkAdminStatus = useCallback(async () => {
         try {
@@ -54,6 +61,12 @@ export default function Game() {
             setIsLoadingAdminStatus(false);
         }
     }, []);
+
+    useEffect(() => {
+        if (gameStatsStatus === 'idle') {
+            dispatch(fetchGameStats())
+        }
+    }, [dispatch, gameStatsStatus]);
 
     useEffect(() => {
         checkAdminStatus();
@@ -110,9 +123,19 @@ export default function Game() {
     const handleCancelButtonPressed = () => {
         setModalVisible(!modalVisible)
     }
-
-    return (
-        <SafeAreaView>
+    let view: JSX.Element = <></>;
+    if (gameStatsStatus === 'loading') {
+        view = <View style={styles.container}>
+            <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+    }
+    else if (gameStatsStatus === 'failed') {
+        view = <View style={styles.container}>
+            <Text>Game stats not found</Text>
+        </View>
+    }
+    else if (gameStatsStatus === 'succeeded') {
+        view = <View style={styles.containerWithoutCentering}>
             <Text style={styles.date}>{formattedDate} at {game.address}</Text>
             
             <View style={styles.gameStatView}> 
@@ -176,6 +199,12 @@ export default function Game() {
                     onSubmit={handleSaveButtonPressed}
                 />
             </Modal>
+        </View>
+    }
+
+    return (
+        <SafeAreaView style={styles.containerWithoutCentering}>
+            {view}
         </SafeAreaView>
     );
 }
@@ -201,7 +230,15 @@ const styles = StyleSheet.create({
     score: { 
         textAlign: 'center',
         fontSize: 48,
-    }, 
+    },
+    containerWithoutCentering: {
+        flex: 1
+    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
     gameStatView: { 
         marginTop: 20,
         marginBottom: 20,
