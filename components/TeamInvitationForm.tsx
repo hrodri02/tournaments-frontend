7d7896b4bcbffda798ea18cf2e23de65cc6c1269
Heri <hrodriguez1821@gmail.com>
2025-10-04 20:47:59 -0700

    Form for adding the email of a player to invite to a team.

---
+++ components/TeamInvitationForm.tsx
@@ -0,0 +1,94 @@
+import React, {useState} from 'react';
+import { View, Text, TextInput, Button, Pressable, StyleSheet } from 'react-native';
+
+interface TeamInvitationFormProps {
+    onInvite: (email: string) => void;
+    onClose: () => void;
+}
+
+export default function TeamInvitationForm({ onInvite, onClose}: TeamInvitationFormProps) {
+    const [email, setEmail] = useState('');
+
+    const handleAddEmailPress = () => {
+        // Clean up the email (e.g., remove whitespace)
+        const trimmedEmail = email.trim();
+        
+        if (trimmedEmail) {
+            // 3. Call the parent's function
+            onInvite(trimmedEmail);
+            
+            // Clear the input and close the modal
+            setEmail(''); 
+            onClose();
+        } else {
+            // Handle case where input is empty
+            alert('Please enter a valid email address.');
+        }
+    };
+
+    return (
+        <View style={styles.modalContainer}>
+            <Text style={styles.modalTitle}>Invite a Friend</Text>
+            <TextInput
+                style={[styles.input, styles.whiteBackground]}
+                onChangeText={setEmail}
+                value={email}
+                placeholder="Enter email"
+            />
+
+            <Pressable
+                onPress={handleAddEmailPress}
+                style={styles.buttonContainer}
+            >
+                <Text style={styles.buttonText}>Add Email</Text>
+            </Pressable>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    modalContainer: {
+        flex: 1,
+        marginTop: 44,
+        borderRadius: 20,
+        padding: 35,
+        backgroundColor: '#777',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        elevation: 5,
+    },
+    modalTitle: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 15
+    },
+    input: {
+        height: 40,
+        borderWidth: 1,
+        padding: 10,
+        marginBottom: 20
+    },
+    whiteBackground: {
+        backgroundColor: 'white'
+    },
+    buttonContainer: {
+        marginHorizontal: 20,
+        borderRadius: 8,
+        backgroundColor: '#007AFF',
+        marginBottom: 20,
+        height: 48,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonText: {
+        textAlign: 'center',
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
+    }
+});
