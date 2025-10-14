9adf5ecc7ec181efa10a173115dc4c212fb249f4
Heri <hrodriguez1821@gmail.com>
2025-10-14 15:14:08 -0700

    Added a reducer to reset the state of a create team request

--- store/teams/teamsSlice.ts
+++ store/teams/teamsSlice.ts
@@ -73,7 +73,10 @@ const teamsSlice = createSlice({
     name: "teams",
     initialState,
     reducers: {
-
+        resetCreateTeamStatus: (state) => {
+            state.createStatus = 'idle'
+            state.createError = null
+        },
     },
     extraReducers: (builder) => {
         builder
@@ -112,6 +115,7 @@ const teamsSlice = createSlice({
     }
 });
 
+export const { resetCreateTeamStatus } = teamsSlice.actions
 export default teamsSlice.reducer;
 
 export const selectTeamsState = (state: RootState) => state.teams
