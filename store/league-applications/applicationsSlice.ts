2a3bc2db64feaf0fbed9db64f5332d8933fa9981
Heri <hrodriguez1821@gmail.com>
2025-11-19 11:05:41 -0800

    Added a thunk to fetch a team's applications and handlers for changes in the fetch state request. Added a reducer to reset the applications fetch state. Added a custom selector to select applications by teamId.

--- store/league-applications/applicationsSlice.ts
+++ store/league-applications/applicationsSlice.ts
@@ -4,9 +4,10 @@ import {
     createSlice,
     createEntityAdapter,
     EntityState,
+    createSelector
 } from "@reduxjs/toolkit";
 import { Application, CreateApplicationRequest } from "@/entities/index";
-import { postApplyToLeague } from "@/services/tournaments.service";
+import { getTeamApplications, postApplyToLeague } from "@/services/tournaments.service";
 
 interface ApplicationsState extends EntityState<Application, number> {
     fetchStatus: "idle" | "loading" | "succeeded" | "failed";
@@ -37,6 +38,14 @@ interface CreateApplicationPayload {
     requestBody: CreateApplicationRequest;
 }
 
+export const fetchTeamApplications = createAppAsyncThunk(
+    "applications/fetchTeamApplications",
+    async (teamId: number) => {
+        const applications = await getTeamApplications(teamId);
+        return applications;
+    }
+);
+
 export const createApplication = createAppAsyncThunk(
     "applications/createApplication",
     async (payload: CreateApplicationPayload) => {
@@ -50,13 +59,37 @@ const applicationsSlice = createSlice({
     name: "applications",
     initialState,
     reducers: {
-        resetCreateApplicationStatus: (state) => {
+        resetApplicationsFetchState: (state) => {
+            state.fetchStatus = 'idle'
+            state.fetchError = null
+        },
+        resetApplicationsCreateState: (state) => {
             state.createStatus = 'idle'
             state.createError = null
         },
     },
     extraReducers(builder) {
         builder
+            .addCase(fetchTeamApplications.pending, (state) => {
+                state.fetchStatus = "loading";
+                state.fetchError = null;
+            })
+            .addCase(fetchTeamApplications.fulfilled, (state, action) => {
+                state.fetchStatus = "succeeded";
+                const applicationResponses = action.payload;
+                const applicationsToStore: Application[] = applicationResponses.map(applicationResponse => {
+                    const {team, league, ...applicationData} = applicationResponse;
+                    const teamId = team.id;
+                    const leagueId = league.id;
+                    const applicationToStore = { teamId, leagueId, ...applicationData };
+                    return applicationToStore;
+                })
+                applicationsAdapter.addMany(state, applicationsToStore);
+            })
+            .addCase(fetchTeamApplications.rejected, (state, action) => {
+                state.fetchStatus = "failed";
+                state.fetchError = action.error.message ?? "Unknown Error";
+            })
             .addCase(createApplication.pending, (state) => {
                 state.createStatus = "loading";
                 state.createError = null;
@@ -77,6 +110,7 @@ const applicationsSlice = createSlice({
     },
 });
 
+export const { resetApplicationsFetchState, resetApplicationsCreateState } = applicationsSlice.actions
 export default applicationsSlice.reducer;
 
 export const selectApplicationsState = (state: RootState) => state.applications
@@ -110,3 +144,9 @@ export const selectApplicationsDeleteStatus = (state: RootState) =>
 
 export const selectApplicationsDeleteError = (state: RootState) =>
     selectApplicationsState(state).deleteError;
+
+// Memoized selector factory to filter applications by teamId
+export const makeSelectApplicationsByTeamId = (teamId: number) =>
+  createSelector([selectAllApplications], (applications) =>
+    applications.filter((application) => application.teamId === teamId)
+);
