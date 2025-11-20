7806967bfc01ff2f0f8c1c922a2e791cdb11cf1a
Heri <hrodriguez1821@gmail.com>
2025-11-20 08:54:37 -0800

    Added menu button to top right navigation bar that is visible to admin users only. Pressing it presents an action sheet with two options: applications and cancel.

--- app/(app)/home/leagues/[id]/index.jsx
+++ app/(app)/home/leagues/[id]/index.jsx
@@ -1,43 +1,30 @@
-import React, { useLayoutEffect, useMemo } from 'react';
-import { FlatList, StyleSheet, View, Text, ActivityIndicator } from 'react-native';
+import React, { useEffect, useLayoutEffect, useMemo } from 'react';
+import { 
+  FlatList, 
+  StyleSheet, 
+  View, 
+  Text, 
+  ActivityIndicator, 
+  Pressable 
+} from 'react-native';
+import { useActionSheet } from '@expo/react-native-action-sheet';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 import { GameExcerpt } from '@/store/leagues/GameExcerpt';
 import { useAppSelector, useAppDispatch } from '@/hooks/useStore';
-import { selectGamesStatus, fetchGames, makeSelectGamesByLeagueId } from '@/store/games/gamesSlice';
+import { 
+  selectGamesStatus, 
+  fetchGames, 
+  makeSelectGamesByLeagueId 
+} from '@/store/games/gamesSlice';
 import { selectLeagueById } from '@/store/leagues/leaguesSlice';
 import { useLocalSearchParams } from 'expo-router';
 import { useNavigation } from '@react-navigation/native';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  item: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-  },
-  header: {
-    fontWeight: 'bold',
-    fontSize: 18,
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    borderBottomWidth: 1
-  },
-  separator: {
-    borderWidth: 0.5,
-    borderColor: 'black'
-  },
-  loadingContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-});
+import { useAuth } from "@/contexts/AuthContext";
 
 export default function LeagueScreen() {
+  const { user, isLoading } = useAuth();
+  const { showActionSheetWithOptions } = useActionSheet();
   const { id } = useLocalSearchParams();
   const navigation = useNavigation();
   const dispatch = useAppDispatch();
@@ -51,7 +38,7 @@ export default function LeagueScreen() {
   const gamesOfLeague = useAppSelector(selectGamesOfLeague);
 
   // fetch games if they haven't already
-  React.useEffect(() => {
+  useEffect(() => {
     if (gamesStatus === 'idle') {
       dispatch(fetchGames(leagueId));
     }
@@ -62,6 +49,49 @@ export default function LeagueScreen() {
       navigation.setOptions({ title: league.name });
     }
   }, [navigation, league?.name]);
+
+  const handlePress = () => {
+    const options = ['Applications', 'Cancel'];
+    const cancelButtonIndex = options.length - 1;
+
+    showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex,
+      },
+      (buttonIndex) => {
+        switch (buttonIndex) {
+          case 0: 
+            console.log('display applications page');
+            break;
+          case 1:
+            break;
+        }
+      }
+    );
+  };
+
+  useLayoutEffect(() => {
+    if (user && !isLoading) {
+      if (user.appUserRole === 'ADMIN') {
+        navigation.setOptions({
+          headerRight: () => (
+            <Pressable
+              style={styles.topRightNavButton}
+              onPress={handlePress}
+            >
+              <FontAwesome6 name="ellipsis-vertical" size={24} color="black" />
+            </Pressable>
+            )
+        });
+      }
+      else {
+        navigation.setOptions({
+          headerRight: undefined
+        });
+      }
+    }
+  }, [navigation, user, isLoading, handlePress])
 
   if (gamesStatus === 'loading') {
     return (
@@ -98,4 +128,35 @@ export default function LeagueScreen() {
     </SafeAreaProvider>
   );
 }
-  
+
+const styles = StyleSheet.create({
+  topRightNavButton: {
+    marginHorizontal: 20,
+  },
+  container: {
+    flex: 1,
+  },
+  item: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  header: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    borderBottomWidth: 1
+  },
+  separator: {
+    borderWidth: 0.5,
+    borderColor: 'black'
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
