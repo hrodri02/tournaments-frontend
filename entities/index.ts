b57849acecc64ba4b0abe4999c1c6aac59ff1de2
Heri <hrodriguez1821@gmail.com>
2025-12-02 13:17:13 -0800

    Moved display maps to components that use them to enable translation.

--- entities/index.ts
+++ entities/index.ts
@@ -2,14 +2,6 @@ import {User} from "@/entities/auth";
 
 export type Position = 'GOAL_KEEPER' | 'DEFENDER' | 'MIDFIELDER' | 'STRIKER' | 'WINGER';
 
-export const PositionDisplay: Record<Position, string> = {
-    'GOAL_KEEPER': 'Goal keeper',
-    'DEFENDER': 'Defender',
-    'MIDFIELDER': 'Midfielder',
-    'STRIKER': 'Striker',
-    'WINGER': 'Winger'
-};
-
 export interface Player extends User {
     position: Position;
 }
@@ -46,13 +38,6 @@ export interface TeamResponse {
 }
 
 export type TeamInviteStatus = 'PENDING' | 'ACCEPTED' | 'DECLINED' | 'REVOKED';
-
-export const TeamInviteStatusDisplay: Record<TeamInviteStatus, string> = {
-    'PENDING': 'Pending',
-    'ACCEPTED': 'Accepted',
-    'DECLINED': 'Declined',
-    'REVOKED': 'Revoked',
-};
 
 export interface CreateTeamInviteRequest {
     email: string;
