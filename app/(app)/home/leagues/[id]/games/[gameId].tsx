ce189cd699715641196847e3fd9fdba40dba62e5
Heri <hrodriguez1821@gmail.com>
2025-12-09 14:44:26 -0800

    In the game stat slice store the entire error object returned from http requests. In the game detail page, display an appropriate error message using the error objects for when fetch and create requests fail.

--- app/(app)/home/leagues/[id]/games/[gameId].tsx
+++ app/(app)/home/leagues/[id]/games/[gameId].tsx
@@ -23,9 +23,11 @@ import { makeSelectDenormalizedGame } from '@/store/games/gamesSlice';
 import { 
     fetchGameStats, 
     makeSelectDenormalizedStats,
-    selectGameStatsFetchStatus, 
+    selectGameStatsFetchStatus,
+    selectGameStatsFetchError, 
     createGameStat, 
     selectGameStatsCreateStatus, 
+    selectGameStatsCreateError,
     resetCreateGameStatStatus 
 } from '@/store/gamestats/gameStatsSlice';
 import { format } from 'date-fns';
@@ -47,7 +49,7 @@ const screenHeight = Dimensions.get('window').height;
 
 export default function Game() {
     const { user, isLoading } = useAuth();
-    const { t, i18n } = useTranslation(['home', 'common']);
+    const { t, i18n } = useTranslation(['home', 'common', 'errors']);
     const [modalVisible, setModalVisible] = useState(false);
     const [editModalVisible, setEditModalVisible] = useState(false);
     const dispatch = useAppDispatch()
@@ -65,7 +67,9 @@ export default function Game() {
     const locale = (['en', 'en-US'].includes(i18n.language))? enUS : es;
     const formattedDate = format(game.gameDateTime, 'eee, MMM d', {locale: locale});
     const gameStatsStatus = useAppSelector(selectGameStatsFetchStatus)
+    const fetchError = useAppSelector(selectGameStatsFetchError);
     const createStatus = useAppSelector(selectGameStatsCreateStatus)
+    const createError = useAppSelector(selectGameStatsCreateError);
     const selectGameStatsOfGame = useMemo(
         () => makeSelectDenormalizedStats(gameID),
         []
@@ -120,6 +124,7 @@ export default function Game() {
     };
 
     useLayoutEffect(() => {
+        console.log(user)
         if (user && !isLoading) {
             if (user.appUserRole === 'ADMIN' && isGameActive(game)) {
                 navigation.setOptions({
@@ -178,15 +183,27 @@ export default function Game() {
         setEditModalVisible(!editModalVisible)
     }
 
+    const getFetchGameStatsErrorMessage = (): string => {
+        const error = fetchError!
+        const message = t(`errors:${error.errorKey}`);
+        return message;
+    }
+
+    const getCreateGameStatErrorMessage = (): string => {
+        const error = createError!
+        const message = t(`errors:${error.errorKey}`);
+        return message;
+    }
+
     let view: React.JSX.Element = <></>;
     if (gameStatsStatus === 'loading' || createStatus === 'loading') {
         view = <ActivityIndicator size="large" color="#0000ff" />
     }
     else if (gameStatsStatus === 'failed') {
-        view = <Text>Game stats not found</Text>
+        view = <Text>{getFetchGameStatsErrorMessage()}</Text>
     }
     else if (createStatus === 'failed') {
-        view = <Text>Could not create stat</Text>
+        view = <Text>{getCreateGameStatErrorMessage()}</Text>
     }
     else if (gameStatsStatus === 'succeeded') {
         view = <View>
