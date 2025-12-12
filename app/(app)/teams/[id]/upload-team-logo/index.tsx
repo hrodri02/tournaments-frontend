7cd2d14f5304fafe6812c578742abb440b70dc51
Heri <hrodriguez1821@gmail.com>
2025-12-12 09:50:18 -0800

    Created a page for uploading a team logo and added a stack screen for it in the teams layout.

---
+++ app/(app)/teams/[id]/upload-team-logo/index.tsx
@@ -0,0 +1,215 @@
+import React, { useState, useEffect } from 'react';
+import { useLocalSearchParams, router } from 'expo-router';
+import {
+    View,
+    Text, 
+    Pressable, 
+    Image, 
+    Alert, 
+    StyleSheet,
+    ActivityIndicator
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Ionicons from '@expo/vector-icons/Ionicons'; 
+import * as ImagePicker from 'expo-image-picker';
+import { useAppDispatch, useAppSelector } from '@/hooks/useStore';
+import { 
+    updateTeamRequest,
+    selectTeamById,
+    selectTeamsUpdateStatus, 
+    selectTeamsUpdateError,
+    resetUpdateTeamState
+} from '@/store/teams/teamsSlice';
+import { uploadImageToS3 } from '@/services/tournaments.service';
+import { useTranslation } from 'react-i18next'; 
+import { CreateTeamRequest } from '@/entities';
+
+export default function UploadTeamLogo() {
+    const [imageUri, setImageUri] = useState<string | null>(null);
+    const [s3error, setS3error] = useState<string | null>(null);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const { t } = useTranslation(['teams', 'errors']);
+    const { id } = useLocalSearchParams();
+    const teamId = Number(id);
+    const dispatch = useAppDispatch();
+    const updateStatus = useAppSelector(selectTeamsUpdateStatus);
+    const updateError = useAppSelector(selectTeamsUpdateError);
+    const team = useAppSelector(state => selectTeamById(state, teamId));
+
+    const pickImage = async () => {
+        // 1. Request permission to access the media library
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        
+        if (status !== 'granted') {
+            Alert.alert(
+                'Permission Denied',
+                'Sorry, we need camera roll permissions to make this work!'
+            );
+            return;
+        }
+
+        // 2. Launch the image picker
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ['images'], // Only allow images
+            allowsEditing: true, // Allow user to crop/edit the image
+            aspect: [4, 3], // Set the aspect ratio for editing (e.g., a standard photo aspect)
+            quality: 1, // Max quality
+        });
+
+        // 3. Handle the result
+        if (!result.canceled) {
+            // The result object has an 'assets' array when 'allowsMultipleSelection' is false
+            // and we take the first asset's URI.
+            setImageUri(result.assets[0].uri);
+            console.log('Selected Image URI:', result.assets[0].uri);
+            // NOTE: This URI is a local path on the device, perfect for immediate display or for uploading.
+        }
+    };
+
+    const uploadImage = async () => {
+        try {
+            setIsLoading(true);
+            const imageUrl = await uploadImageToS3(imageUri!);
+            const now: Date = new Date();
+            const isoString: string = now.toISOString();
+            const updatedTeam: CreateTeamRequest = {
+                name: team.name,
+                logoUrl: imageUrl,
+                playersToInvite: [],
+                createdAt: isoString
+            };
+            dispatch(updateTeamRequest({teamId: team.id, updatedTeam: updatedTeam}));
+        }
+        catch (err) {
+            setIsLoading(false);
+            const translationKey = err.message;
+            setS3error(t(translationKey));
+        }
+    }
+
+    useEffect(() => {
+        if (updateStatus === 'succeeded') {
+            setIsLoading(false);
+            // slight delay to show success:
+            const timer = setTimeout(() => {
+                dispatch(resetUpdateTeamState())
+                router.back();
+            }, 3000);
+            return () => clearTimeout(timer);
+        }
+    }, [updateStatus]);
+
+
+    const getUpdateErrorMessage = (): string => {
+        const error = updateError!;
+        const message = t(`errors:${error.errorKey}`);
+        return message;
+    }
+
+    const skipUpload = () => {
+        router.back();
+    }
+
+    let view: React.JSX.Element = <></>;
+    if (isLoading) {
+        view = <View style={[styles.container, styles.perfectCentering]}>
+            <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+    }
+    else if (updateStatus === 'idle' || updateStatus === 'failed') {
+        view = <View style={styles.container}>
+            <View style={[styles.container, styles.perfectCentering]}>
+                <Pressable onPress={pickImage}>
+                    <Text style={styles.subtext}>{t('upload_team_logo.message')}</Text>
+                </Pressable>
+                {imageUri && (
+                    <Image 
+                        source={{ uri: imageUri }} 
+                        style={styles.image} 
+                    />
+                )}
+                {updateError && <Text style={styles.errorText}>{getUpdateErrorMessage()}</Text>}
+                {s3error && <Text style={styles.errorText}>{s3error}</Text>}
+            </View>
+            <Pressable 
+                onPress={uploadImage} 
+                style={[styles.buttonContainer, imageUri === null && styles.buttonDisabled]}
+                disabled={imageUri === null}
+            >
+                <Text style={styles.buttonText}>{t('upload_team_logo.button')}</Text>
+            </Pressable>
+            <Pressable onPress={skipUpload}>
+                <Text style={styles.centerText}>{t('upload_team_logo.skip_button')}</Text>
+            </Pressable>
+        </View>
+    }
+    else if (updateStatus === 'succeeded') {
+        view = <View style={[styles.container, styles.perfectCentering]}>
+            <Text>{t('upload_team_logo.success_message')}</Text>
+            <Ionicons name="checkmark-circle" size={32} color="green" />
+        </View>
+    }
+
+    return (
+        <SafeAreaView style={styles.safeAreaContainer}>
+            {view}
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    safeAreaContainer: {
+        flex: 1
+    },
+    container: {
+        flex: 1,
+        margin: 12
+    },
+    perfectCentering: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    text: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 15
+    },
+    subtext: {
+        fontSize: 16,
+    },
+    image: {
+        width: 200,
+        height: 200,
+        borderRadius: 100,
+        marginTop: 20,
+        borderWidth: 1,
+        borderColor: '#ddd',
+    },
+    buttonContainer: {
+        marginHorizontal: 20,
+        borderRadius: 8,
+        backgroundColor: '#007AFF',
+        marginBottom: 20,
+        height: 48,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    buttonDisabled: {
+        backgroundColor: '#ccc',
+    },
+    buttonText: {
+        textAlign: 'center',
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    errorText: {
+        color: 'red',
+        textAlign: 'center',
+        fontSize: 12,
+        marginBottom: 5,
+    },
+    centerText: {
+        textAlign: 'center'
+    }
+});
