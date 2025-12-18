7feff9bb49471ee9e0547715ea7fd564634dc3a3
Heri <hrodriguez1821@gmail.com>
2025-12-18 07:59:47 -0800

    When create league button is pressed make post request to backend and track the state of the request to update the UI. Added translation for the success message.

--- app/(app)/home/create-league/index.tsx
+++ app/(app)/home/create-league/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { router } from 'expo-router';
+import React, { useEffect } from 'react';
 import { 
     StyleSheet, 
     Text, 
@@ -7,11 +8,20 @@ import {
     Pressable,
     ActivityIndicator,
 } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons'; 
 import { SafeAreaView } from 'react-native-safe-area-context';
 import UniversalDatePicker from '@/components/UniversalDatePicker';
 import DismissKeyboard from '@/components/DismissKeyboard';
 import { useForm, Controller } from 'react-hook-form';
+import { useAppDispatch, useAppSelector } from '@/hooks/useStore';
+import { 
+    selectLeaguesCreateStatus, 
+    selectLeaguesCreateError,
+    createLeague,
+    resetLeaguesCreateState
+} from '@/store/leagues/leaguesSlice';
 import { useTranslation } from 'react-i18next';
+import { CreateLeagueRequest } from '@/entities';
 
 interface CreateLeagueFormData {
     leagueName: string;
@@ -20,6 +30,9 @@ interface CreateLeagueFormData {
 }
 
 export default function CreateLeague() {
+    const dispatch = useAppDispatch();
+    const createStatus = useAppSelector(selectLeaguesCreateStatus);
+    const createError = useAppSelector(selectLeaguesCreateError);
     const { t } = useTranslation(['home', 'errors']);
     const { control, handleSubmit } = useForm<CreateLeagueFormData>({
         defaultValues: {
@@ -30,78 +43,123 @@ export default function CreateLeague() {
     });
 
     function onCreateLeagueButtonPressed(data: CreateLeagueFormData) {
-        console.log(data);
+        const createLeagueRequest: CreateLeagueRequest = {
+            name: data.leagueName,
+            startDate: data.leagueStartDate.toISOString(),
+            durationInWeeks: Number(data.leagueDuration)
+        };
+        dispatch(createLeague(createLeagueRequest));
+    }
+
+    useEffect(() => {
+        if (createStatus === 'succeeded' || createStatus === 'failed') {
+            // slight delay to show success:
+            const timer = setTimeout(() => {
+                dispatch(resetLeaguesCreateState())
+                if (createStatus === 'succeeded') {
+                    router.back();
+                }
+            }, 3000);
+            return () => clearTimeout(timer);
+        }
+    }, [createStatus]);
+
+    const getCreateErrorMessage = (): string => {
+        const error = createError!
+        const message = t(`errors:${error.errorKey}`);
+        return message;
+    }
+
+    let view: React.JSX.Element = <></>;
+    if (createStatus === 'idle' || createStatus === 'failed') {
+        view = <DismissKeyboard>
+            <View style={[styles.container, styles.marginAtTop]}>
+                <View style={styles.container}>
+                    {createError && <Text style={styles.errorText}>{getCreateErrorMessage()}</Text>}
+                    <Controller
+                        control={control}
+                        name="leagueName"
+                        rules={{ required: 'League name is required' }}
+                        render={({ field: { onChange, value }, fieldState: { error } }) => (
+                            <View style={styles.marginAtBottom}>
+                                <Text style={styles.label}>{t('create_league.name_label')}</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    onChangeText={onChange}
+                                    value={value}
+                                    placeholder={t('create_league.name_placeholder')}
+                                    placeholderTextColor="#AAAAAA"
+                                />
+                                {error && <Text style={styles.errorText}>{error.message}</Text>}
+                            </View>
+                        )}
+                    />
+                    <Controller
+                        control={control}
+                        name="leagueDuration"
+                        rules={{ required: 'League duration is required' }}
+                        render={({ field: { onChange, value }, fieldState: { error } }) => (
+                            <View style={styles.marginAtBottom}>
+                                <Text style={styles.label}>{t('create_league.duration_label')}</Text>
+                                <TextInput
+                                    style={styles.input}
+                                    onChangeText={onChange}
+                                    value={value}
+                                    keyboardType="numeric" 
+                                    placeholder={t('create_league.duration_placeholder')}
+                                    placeholderTextColor="#AAAAAA"
+                                />
+                                {error && <Text style={styles.errorText}>{error.message}</Text>}
+                            </View>
+                        )}
+                    />
+                    <Controller
+                        control={control}
+                        name="leagueStartDate"
+                        rules={{ required: 'League duration is required' }}
+                        render={({ field: { onChange, value }, fieldState: { error } }) => (
+                            <View style={styles.marginAtBottom}>
+                                <Text style={styles.label}>{t('create_league.start_date_label')}</Text>
+                                <UniversalDatePicker
+                                    testID="dateTimePicker"
+                                    value={value}
+                                    mode={'date'}
+                                    is24Hour={true}
+                                    onChange={(event, selectedDate) => {
+                                        if (selectedDate) {
+                                            onChange(selectedDate); 
+                                        }
+                                    }}
+                                />
+                            </View>
+                        )}
+                    />
+                </View>
+                <Pressable
+                    onPress={handleSubmit(onCreateLeagueButtonPressed)}
+                    style={styles.buttonContainer}
+                >
+                    <Text style={styles.buttonText}>{t('create_league.button')}</Text>
+                </Pressable>
+            </View>
+        </DismissKeyboard>
+    }
+    else if (createStatus === 'loading') {
+        view = <View style={[styles.container, styles.perfectCentering]}>
+            <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+    }
+    else if (createStatus === 'succeeded') {
+        view = <View style={[styles.container, styles.perfectCentering]}>
+            <Text>{t('create_team.success_message')}</Text>
+            <Ionicons name="checkmark-circle" size={32} color="green" />
+        </View>
     }
 
     return (
-        <DismissKeyboard>
-            <SafeAreaView style={styles.container}>
-                <View style={[styles.container, styles.marginAtTop]}>
-                    <View style={styles.container}>
-                        <Controller
-                            control={control}
-                            name="leagueName"
-                            rules={{ required: 'League name is required' }}
-                            render={({ field: { onChange, value }, fieldState: { error } }) => (
-                                <View style={styles.marginAtBottom}>
-                                    <Text style={styles.label}>{t('create_league.name_label')}</Text>
-                                    <TextInput
-                                        style={styles.input}
-                                        onChangeText={onChange}
-                                        value={value}
-                                        placeholder={t('create_league.name_placeholder')}
-                                        placeholderTextColor="#AAAAAA"
-                                    />
-                                    {error && <Text style={styles.errorText}>{error.message}</Text>}
-                                </View>
-                            )}
-                        />
-                        <Controller
-                            control={control}
-                            name="leagueDuration"
-                            rules={{ required: 'League duration is required' }}
-                            render={({ field: { onChange, value }, fieldState: { error } }) => (
-                                <View style={styles.marginAtBottom}>
-                                    <Text style={styles.label}>{t('create_league.duration_label')}</Text>
-                                    <TextInput
-                                        style={styles.input}
-                                        onChangeText={onChange}
-                                        value={value}
-                                        keyboardType="numeric" 
-                                        placeholder={t('create_league.duration_placeholder')}
-                                        placeholderTextColor="#AAAAAA"
-                                    />
-                                    {error && <Text style={styles.errorText}>{error.message}</Text>}
-                                </View>
-                            )}
-                        />
-                        <Controller
-                            control={control}
-                            name="leagueStartDate"
-                            rules={{ required: 'League duration is required' }}
-                            render={({ field: { onChange, value }, fieldState: { error } }) => (
-                                <View style={styles.marginAtBottom}>
-                                    <Text style={styles.label}>{t('create_league.start_date_label')}</Text>
-                                    <UniversalDatePicker
-                                        testID="dateTimePicker"
-                                        value={value}
-                                        mode={'date'}
-                                        is24Hour={true}
-                                        onChange={onChange}
-                                    />
-                                </View>
-                            )}
-                        />
-                    </View>
-                    <Pressable
-                        onPress={handleSubmit(onCreateLeagueButtonPressed)}
-                        style={styles.buttonContainer}
-                    >
-                        <Text style={styles.buttonText}>{t('create_league.button')}</Text>
-                    </Pressable>
-                </View>
-            </SafeAreaView>
-        </DismissKeyboard>
+        <SafeAreaView style={styles.container}>
+            {view}      
+        </SafeAreaView>
     );
 }
 
@@ -114,6 +172,10 @@ const styles = StyleSheet.create({
     },
     marginAtBottom: {
         marginBottom: 20
+    },
+    perfectCentering: {
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     label: {
         fontSize: 16,
