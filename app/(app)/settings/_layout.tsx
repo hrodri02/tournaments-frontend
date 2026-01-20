139fe0a618d56a7fd3cd9e1c1aa148c91289fd82
Heri <hrodriguez1821@gmail.com>
2026-01-20 08:35:22 -0800

    Added stack screen for privacy policy page.

--- app/(app)/settings/_layout.tsx
+++ app/(app)/settings/_layout.tsx
@@ -19,6 +19,14 @@ export default function SettingsTabLayout() {
                     headerBackButtonDisplayMode: 'minimal'
                 }}
             />
+            <Stack.Screen 
+                name="privacy-policy/index"
+                options={{
+                    title: t('settings:privacy_policy.title'),
+                    headerShown: true,
+                    headerBackButtonDisplayMode: 'minimal'
+                }}
+            />
         </Stack>
     );
 }
