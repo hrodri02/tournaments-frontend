5d98bb5e5b3c5ca25e72d08af7851aa7078a03cd
Heri <hrodriguez1821@gmail.com>
2026-03-31 10:47:56 -0700

    Moved default images to a single file, so that they are only resolved once by Metro.

---
+++ constants/Assets.ts
@@ -0,0 +1,6 @@
+export const DEFAULT_IMAGES = {
+  LEAGUE_LOGO: require('@/assets/images/default_league_logo.png'),
+  TEAM_LOGO: require('@/assets/images/default_team_logo.png'),
+  SOCCER_BALL: require('@/assets/images/soccerBall.png'),
+  FIELD_BG: require('@/assets/images/soccer_field.jpg'),
+} as const;
